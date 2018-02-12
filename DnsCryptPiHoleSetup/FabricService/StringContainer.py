a2228ab0ff2520716c6a324ff5390e7549c27205
john <jmcgrath207@gmail.com>
2018-02-11 23:55:21 -0600

    Updated the String Container

--- DnsCryptPiHoleSetup/FabricService/StringContainer.py
+++ DnsCryptPiHoleSetup/FabricService/StringContainer.py
@@ -1,12 +1,16 @@
 
 
 DnsCryptSocket = """
-[Unit]
 Description=dnscrypt-proxy listening socket
+Documentation=https://github.com/jedisct1/dnscrypt-proxy/wiki
+Before=nss-lookup.target
+Wants=nss-lookup.target
 
 [Socket]
 ListenStream={0}:41
 ListenDatagram={0}:41
+NoDelay=true
+DeferAcceptSec=1
 
 [Install]
 WantedBy=sockets.target
@@ -14,59 +18,27 @@ WantedBy=sockets.target
 
 
 DnsCryptService = """
-[Unit]
 Description=DNSCrypt client proxy
-Documentation=man:dnscrypt-proxy(8)
-Requires=dnscrypt-proxy@%i.socket
+Documentation=https://github.com/jedisct1/dnscrypt-proxy/wiki
+Requires=dnscrypt-proxy.socket
 After=network.target
 Before=nss-lookup.target
-
-[Install]
-Also=dnscrypt-proxy@%i.socket
-WantedBy=multi-user.target
+Wants=nss-lookup.target
 
 [Service]
 Type=simple
 NonBlocking=true
-
-# Fill in the resolver name with one from dnscrypt-resolvers.csv file
-# It is also recommended to create a dedicated system user, for example _dnscrypt
-# Additional features, such as ephemeral keys and plugins, can be enabled here as well
-ExecStart=/usr/local/sbin/dnscrypt-proxy \
-        --resolver-name=%i \
-        --user=dnscrypt 
-
-Restart=always
-RestartSec=10
-"""
-
-DnsCryptServiceEphemeral = """
-[Unit]
-Description=DNSCrypt client proxy
-Documentation=man:dnscrypt-proxy(8)
-Requires=dnscrypt-proxy@%i.socket
-After=network.target
-Before=nss-lookup.target
+ExecStart=/usr/bin/dnscrypt-proxy --config /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+DynamicUser=yes
+ProtectHome=true
+CacheDirectory=dnscrypt-proxy
+LogsDirectory=dnscrypt-proxy
 
 [Install]
-Also=dnscrypt-proxy@%i.socket
 WantedBy=multi-user.target
-
-[Service]
-Type=simple
-NonBlocking=true
-
-# Fill in the resolver name with one from dnscrypt-resolvers.csv file
-# It is also recommended to create a dedicated system user, for example _dnscrypt
-# Additional features, such as ephemeral keys and plugins, can be enabled here as well
-ExecStart=/usr/local/sbin/dnscrypt-proxy \
-        --resolver-name=%i \
-        --ephemeral-keys \
-        --user=dnscrypt
-
-Restart=always
-RestartSec=10
 """
+
+
 
 
 DnsCryptConf = """
@@ -78,11 +50,11 @@ DnsCryptConf = """
 
 DnsCryptSudoer = """
 
-
-%dnscrypt ALL= NOPASSWD: /bin/systemctl start dnscrypt-proxy@*
-%dnscrypt ALL= NOPASSWD: /bin/systemctl stop dnscrypt-proxy@*
-%dnscrypt ALL= NOPASSWD: /bin/systemctl status dnscrypt-proxy@*
-%dnscrypt ALL= NOPASSWD: /bin/systemctl restart dnscrypt-proxy@*
-%dnscrypt ALL= NOPASSWD: /bin/journalctl --since*-u  dnscrypt-proxy@* 
+%dnscrypt ALL= NOPASSWD: /usr/bin/dnscrypt-proxy dnscrypt-proxy*
+%dnscrypt ALL= NOPASSWD: /bin/systemctl start dnscrypt-proxy*
+%dnscrypt ALL= NOPASSWD: /bin/systemctl stop dnscrypt-proxy*
+%dnscrypt ALL= NOPASSWD: /bin/systemctl status dnscrypt-proxy*
+%dnscrypt ALL= NOPASSWD: /bin/systemctl restart dnscrypt-proxy*
+%dnscrypt ALL= NOPASSWD: /bin/journalctl --since*-u  dnscrypt-proxy* 
 
 """
